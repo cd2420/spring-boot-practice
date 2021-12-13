5a0130cfa6495d3d918c1287160c1b0a8d8ad716
imdaegeun <cd3818@naver.com>
2021-12-14 00:32:10 +0900

    12/14 - DMaker 데이터 리스트 가져오는 기능 구현

--- dmaker/src/main/java/com/example/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/com/example/dmaker/controller/DMakerController.java
@@ -1,16 +1,15 @@
 package com.example.dmaker.controller;
 
 import com.example.dmaker.dto.CreateDeveloper;
+import com.example.dmaker.dto.DeveloperDto;
 import com.example.dmaker.service.DMakerService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,6 +22,11 @@ public class DMakerController {
     public CreateDeveloper.Response createDeveloper(@Valid @RequestBody final CreateDeveloper.Request request){
         log.info("request = {}", request);
         return dMakerService.createDeveloper(request);
+    }
+
+    @GetMapping("/developer")
+    public List<DeveloperDto> getAllDeveloper() {
+        return dMakerService.getAllEmployedDeveloper();
     }
 
 }
--- dmaker/src/main/java/com/example/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/example/dmaker/service/DMakerService.java
@@ -1,6 +1,7 @@
 package com.example.dmaker.service;
 
 import com.example.dmaker.dto.CreateDeveloper;
+import com.example.dmaker.dto.DeveloperDto;
 import com.example.dmaker.entity.Developer;
 import com.example.dmaker.exception.DMakerException;
 import com.example.dmaker.repository.DeveloperRepository;
@@ -9,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.example.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
 import static com.example.dmaker.type.StatusCode.EMPLOYED;
@@ -39,6 +43,7 @@ public class DMakerService {
                 .build();
     }
 
+    @Transactional(readOnly = true)
     private void validateCreateDeveloperRequest(@NonNull CreateDeveloper.Request request) {
         request.getDeveloperLevel().validateExperienceYears(request.getExperienceYear());
 
@@ -46,5 +51,12 @@ public class DMakerService {
                 .ifPresent((developer -> {
                     throw new DMakerException(DUPLICATED_MEMBER_ID);
                 }));
+    }
+
+    @Transactional(readOnly = true)
+    public List<DeveloperDto> getAllEmployedDeveloper() {
+        return developerRepository.findDevelopersByStatusCodeEquals(EMPLOYED)
+                .stream().map(DeveloperDto::fromEntity)
+                        .collect(Collectors.toList());
     }
 }
