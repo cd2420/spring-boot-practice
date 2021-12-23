a61224fba19db0b875c4c69c0ce9eae4a2a8477e
imdaegeun <cd3818@naver.com>
2021-12-24 00:06:34 +0900

    12/24 - config batch

--- point/src/main/java/com/example/point/PointApplication.java
+++ point/src/main/java/com/example/point/PointApplication.java
@@ -1,12 +1,15 @@
 package com.example.point;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PointApplication {
 
 	public static void main(String[] args) {
+		log.info("application arguments : " + String.join(",", args));
 		SpringApplication.run(PointApplication.class, args);
 	}
 
