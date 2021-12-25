3848cd6bfcfd771e33b52f86c1c482d611fa4306
imdaegeun <cd3818@naver.com>
2021-12-25 17:14:25 +0900

    12/25 - error발생 > org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException: A job instance already exists and is complete - 해결 > repository table이 이미 생성되어있기 때문에 JobParameters jobParameters = new JobParametersBuilder()                 .addString("today", "2021-12-27")                 .addLong("currTime", System.currentTimeMillis())                 .toJobParameters();              currTime같이 변하는 값을 추가해준다.

---
+++ point/src/test/java/com/example/point/config/ExpirePointJobConfigurationTest.java
@@ -0,0 +1,60 @@
+package com.example.point.config;
+
+import com.example.point.PointApplicationTests;
+import com.example.point.entity.Point;
+import com.example.point.entity.PointWallet;
+import com.example.point.repository.PointRepository;
+import com.example.point.repository.PointWalletRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpirePointJobConfigurationTest extends PointApplicationTests {
+
+    @Autowired
+    Job expirePointJob;
+
+    @Autowired
+    PointWalletRepository pointWalletRepository;
+
+    @Autowired
+    PointRepository pointRepository;
+
+    @Test
+    void expirePointJob() throws Exception {
+        //given
+        LocalDate earnDate = LocalDate.of(2021, 12, 25);
+        LocalDate expireDate = LocalDate.of(2021,12,26);
+        PointWallet pointWallet = pointWalletRepository.save(
+                new PointWallet(
+                        "user123",
+                        Long.valueOf(6000)
+                )
+        );
+        pointRepository.save(new Point(pointWallet, Long.valueOf(1000), earnDate, expireDate));
+        pointRepository.save(new Point(pointWallet, Long.valueOf(1000), earnDate, expireDate));
+        pointRepository.save(new Point(pointWallet, Long.valueOf(1000), earnDate, expireDate));
+
+        //when
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("today", "2021-12-27")
+                .addLong("currTime", System.currentTimeMillis())
+                .toJobParameters();
+        JobExecution jobExecution = launchJob(expirePointJob, jobParameters);
+
+        //then
+        List<Point> points = pointRepository.findAll();
+        then(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
+        then(points.stream().filter(Point::expire)).hasSize(3);
+        PointWallet changePointWallet = pointWalletRepository.findById(pointWallet.getId()).orElseGet(null);
+        then(changePointWallet).isNotNull();
+        then(changePointWallet.getAmount()).isEqualByComparingTo(Long.valueOf(3000));
+
+    }
+}
