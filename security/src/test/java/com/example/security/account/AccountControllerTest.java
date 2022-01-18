7fcdb096291e20346baffd65c0669bc66b8bfb00
imdaegeun <cd3818@naver.com>
2022-01-18 22:48:45 +0900

    1/18
    - spring security test시 필요한 의존성주입
    - spring secuirty test 코드 작성

---
+++ security/src/test/java/com/example/security/account/AccountControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.security.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index_anonymous() throws Exception {
+        mockMvc.perform(get("/").with(anonymous()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_user() throws Exception {
+        mockMvc.perform(get("/").with(user("lim").roles("USER"))) // lim이라는 유저가 로그인 되어 있는 상태라면~~
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void admin_user() throws Exception {
+        mockMvc.perform(get("/admin").with(user("admin").roles("USER"))) // lim이라는 유저가 로그인 되어 있는 상태라면~~
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void admin_admin() throws Exception {
+        mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN"))) // lim이라는 유저가 로그인 되어 있는 상태라면~~
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
+}
