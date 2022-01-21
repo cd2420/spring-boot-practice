d8454a0cb2a3cefb96e4293dcf0865d5c6a87720
imdaegeun <cd3818@naver.com>
2022-01-21 23:22:45 +0900

    1/21
    - @async 사용해서 비동기적 실행
     - !! 문제점 발생
       - @async사용할 경우 기존 Thread의 하위 Thread가 생성되는데 거기서 SecurityContextHolder를 공유해서 사용하지 못함, 그래서 service 부분의 SecurityLogger에서 NullPointerException 발생
    
       > 해결:
         - SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL) 설정 해줌으로서 하위 Thread에서도 SecurityContextHolder 공유해준다는거 설정

--- security/src/main/java/com/example/security/config/SecurityConfig.java
+++ security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
 import java.util.Arrays;
@@ -32,6 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
                 .and()
             .httpBasic();
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
     @Override
