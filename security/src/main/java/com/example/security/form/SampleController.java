2e9b07039842182ed6dc481be1bc9b8475b29d5d
imdaegeun <cd3818@naver.com>
2022-01-21 23:11:50 +0900

    1/21
    - async

--- security/src/main/java/com/example/security/form/SampleController.java
+++ security/src/main/java/com/example/security/form/SampleController.java
@@ -2,12 +2,16 @@ package com.example.security.form;
 
 import com.example.security.account.AccountContext;
 import com.example.security.account.AccountRepository;
+import com.example.security.common.SecurityLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SampleController {
@@ -53,5 +57,22 @@ public class SampleController {
     public String user(Model model, Principal principal) {
         model.addAttribute("message", "Hello User, " + principal.getName());
         return "admin";
+    }
+
+    /**
+     * async관련 테스트
+     */
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler() {
+        SecurityLogger.log("MVC");
+
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                SecurityLogger.log("Callable"); // 위에랑 다른 별도의 Thread가 동작
+                return "Async Handler";
+            }
+        };
     }
 }
