9dd9a22e7d27f440b75636372f4c925a3830a1a9
imdaegeun <cd3818@naver.com>
2022-01-22 22:21:31 +0900

    1/22
    - csrf test시 설정방법

---
+++ security/src/test/java/com/example/security/account/SignUpControllerTest.java
@@ -0,0 +1,43 @@
+package com.example.security.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SignUpControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void signUpForm() throws Exception {
+        mockMvc.perform(get("/signup"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("_csrf")));
+
+    }
+
+    @Test
+    public void processSignUp() throws Exception {
+        mockMvc.perform(post("/signup")
+                .param("username", "lim")
+                .param("password", "123")
+                .with(csrf()))  // csrf 토큰을 넣어줌
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+
+}
